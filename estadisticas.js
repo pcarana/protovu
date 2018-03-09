760aed8796d586cf56011628dbf5014f46ce0d1d
fmoreno <fmoreno@FMORENO.nic.com.mx>
2018-03-09 16:24:11 -0600

    Add new graphs

--- estadisticas.js
+++ estadisticas.js
@@ -1,21 +1,73 @@
+var chartLabels = ['January', 'February', 'March', 'April', 'May', 'June'];
+var chartData = [40, 39, 20, 40, 39, 80];
+var chartBackgrounds = ['red','yellow','blue','green','purple','black'];
+
 Vue.component('line-chart', {
   extends: VueChartJs.Line,
   mounted () {
     this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: chartLabels,
       datasets: [
         {
           label: 'Data One',
           backgroundColor: '#f87979',
-          data: [40, 39, 10, 40, 39, 80, 40]
+          data: chartData
         }
       ]
     }, {responsive: true, maintainAspectRatio: false})
   }
 });
 
-var data = {} ;
+Vue.component('pie-chart', {
+	  extends: VueChartJs.Pie,
+	  mounted () {
+	    this.renderChart({
+	      labels: chartLabels,
+	      datasets: [
+	        {
+	          label: 'Data One',
+	          backgroundColor: chartBackgrounds,
+	          data: chartData
+	        }
+	      ],
+	    }, {responsive: true, maintainAspectRatio: false})
+	  }
+	});
+
+Vue.component('bar-chart', {
+	  extends: VueChartJs.Bar,
+	  mounted () {
+	    this.renderChart({
+	      labels: chartLabels,
+	      datasets: [
+	        {
+	          label: 'Test Dataset',
+	          backgroundColor: chartBackgrounds,
+	          data: chartData,
+	        }
+	      ],
+	    },
+	    {
+	    	responsive: true,
+	    	maintainAspectRatio: false,
+	    	scales:
+	    	{
+	    		yAxes: [{
+	    	        ticks: {
+	    	          min: 0
+	    	        }
+	    	    }],
+	    	}
+	    })	    
+	  }
+	});
+
+var data = {
+		chart: ''
+};
 
 var methods = {
-		showGraphLine() {}
+		showGraph(type) {
+			this.chart = type;
+		}
 };
