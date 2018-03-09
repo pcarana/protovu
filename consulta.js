225a0ce40566e82c9d17f46ad8d215885137d3a0
fmoreno <fmoreno@FMORENO.nic.com.mx>
2018-03-09 17:51:09 -0600

    Add more data and filter

--- consulta.js
+++ consulta.js
@@ -1,15 +1,23 @@
-var data = {
-    form: {
-        name: '',
-        lastName: ''
-    },
-	tableItems: [
-      { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-      { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-      { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-      { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-	],
-	tableFields: [
+const items = [
+    { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+    { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+    { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+    { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' },
+    { isActive: true, age: 41, first_name: 'ADickerson', last_name: 'ZMacdonald' },
+    { isActive: false, age: 22, first_name: 'BLarsen', last_name: 'YShaw' },
+    { isActive: false, age: 90, first_name: 'CGeneva', last_name: 'XWilson' },
+    { isActive: true, age: 39, first_name: 'DJami', last_name: 'WCarney' },
+    { isActive: true, age: 42, first_name: 'EDickerson', last_name: 'VMacdonald' },
+    { isActive: false, age: 23, first_name: 'FLarsen', last_name: 'UShaw' },
+    { isActive: false, age: 8, first_name: 'GGeneva', last_name: 'TWilson' },
+    { isActive: true, age: 3, first_name: 'HJami', last_name: 'SCarney' },
+    { isActive: true, age: 56, first_name: 'IDickerson', last_name: 'RMacdonald' },
+    { isActive: false, age: 78, first_name: 'JLarsen', last_name: 'QShaw' },
+    { isActive: false, age: 28, first_name: 'KGeneva', last_name: 'PWilson' },
+    { isActive: true, age: 87, first_name: 'LJami', last_name: 'OCarney' }
+	];
+
+const fields = [
 	  {
 		  key: 'first_name',
 		  sortable: true
@@ -22,13 +30,21 @@ var data = {
 		  key: 'age',
 		  sortable: true
 	  }
-	],
-	tableCurrentPage : 1
+	];
+
+var data = {
+	tableItems: items,
+	tableFields: fields,
+	tableCurrentPage : 1,
+	totalRows: items.length,
+	perPage: 4,
+	filter: null
 };
 
 var methods = {
-    onSubmit (evt) {
-        evt.preventDefault();
-        alert(JSON.stringify(this.form));
-      }
+		onFiltered (filteredItems) {
+		    // Trigger pagination to update the number of buttons/pages due to filtering
+		    this.totalRows = filteredItems.length
+		    this.tableCurrentPage = 1
+		}
 };
