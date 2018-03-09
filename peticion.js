731886af043ee2be1138afc43d7ec8c2388dcf70
fmoreno <fmoreno@FMORENO.nic.com.mx>
2018-03-09 17:19:28 -0600

    Add more logic to REST test

--- peticion.js
+++ peticion.js
@@ -1,25 +1,37 @@
 var data = {
     form: {
-      domain: 'test/me'
+      domain: 'test/me',
+      apiresponse: null
     },
-	apiresponse: null
+	apiobject: null,
+	showDismissibleAlert: false,
+	errorMessage: ''
 };
 
 var methods = {
       onSubmitDomain(evt) {
         const vm = this;
+        vm.apiobject = '';
+        vm.form.apiresponse = '';
+        vm.errorMessage = '';
+        vm.showDismissibleAlert = false;
         evt.preventDefault();
         // axios.get('http://localhost:8080/rdap-server/domain/whiterabbit.com')
        // axios.get('https://jsonplaceholder.typicode.com/posts/1')
         axios.get('http://echo.jsontest.com/' + vm.form.domain)
           .then(function (response) {
             console.log(response.data);
-            // vm.apiresponse = JSON.stringify(JSON.parse(response.data), null,
-			// 2);
-            vm.apiresponse = response.data;
+            vm.form.apiresponse = response.data;
+            //vm.apiobject = JSON.parse(JSON.stringify(vm.apiresponse, null, 2));
+            var keys = Object.entries(response.data);
+            for (i = 0; i < keys.length; i++) {
+            	vm.apiobject += keys[i][0] + " = " + keys[i][1] + " / ";
+            }
           })
           .catch(function (error) {
             console.log(error);
+            vm.showDismissibleAlert = true;
+            vm.errorMessage = error;
           });
       }
 };
